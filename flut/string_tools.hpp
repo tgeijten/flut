b8e7b09bc04e2cdd4691a9572b23690693f0559a
Thomas Geijtenbeek <Thomas.Geijtenbeek@MotekMedical.com>
2015-11-09 17:03:25 +0100

    logging updates

--- flut/string_tools.hpp
+++ flut/string_tools.hpp
@@ -9,21 +9,21 @@
 namespace flut
 {
 	/// get left n characters
-	string left_str( const string& str, int i )
+	inline string left_str( const string& str, int i )
 	{ if ( i >= 0 ) return str.substr( 0, size_t( i ) ); else return str.substr( 0, size_t( std::max( 0, int(str.size()) + i ) ) ); }
 
 	/// get middle n characters, starting from pos
-	string mid_str( const string& str, index_t pos, size_t n = string::npos ) { return str.substr( pos, n ); }
+	inline string mid_str( const string& str, index_t pos, size_t n = string::npos ) { return str.substr( pos, n ); }
 
 	/// get middle n characters, starting from pos
-	string right_str( const string& str, int i )
+	inline string right_str( const string& str, int i )
 	{ if ( i >= 0 ) return str.substr( str.size() - i, string::npos ); else return str.substr( size_t( -i ), string::npos ); }
 
 	/// get middle n characters, starting from pos
-	index_t in_str( const string& str, const string& substr, index_t p = 0 ) { return str.find( substr, p ); }
+	inline index_t in_str( const string& str, const string& substr, index_t p = 0 ) { return str.find( substr, p ); }
 
 	/// split a string into a vector of strings
-	std::vector< string > split_str( const std::string& s, const std::string& sep_chars ) {
+	inline std::vector< string > split_str( const std::string& s, const std::string& sep_chars ) {
 		std::vector< std::string > strings;
 		size_t ofs = s.find_first_not_of( sep_chars.c_str(), 0 );
 		while ( ofs != std::string::npos ) {
@@ -35,7 +35,7 @@ namespace flut
 	}
 
 	/// get formatted string (printf style)
-	string stringf( const char* format, ... ) {
+	inline string stringf( const char* format, ... ) {
 		va_list args;
 		va_start( args, format );
 		char buf[ 256 ];
@@ -45,7 +45,7 @@ namespace flut
 	}
 
 	/// Get file extension (without dot)
-	string get_filename_ext( const string& str ) {
+	inline string get_filename_ext( const string& str ) {
 		size_t n = str.find_last_of( '.' );
 		if ( n != std::string::npos ) {
 			std::string ext = str.substr( n + 1 );
@@ -56,17 +56,15 @@ namespace flut
 	}
 
 	/// Get file without extension (without dot)
-	string get_filename_stem( const string& str )
+	inline string get_filename_stem( const string& str )
 	{ return str.substr( 0, str.size() - get_filename_ext( str ).size() ); }
 
 	/// convert any streamable type to string
-	template< typename T >
-	string make_str( const T& value ) { std::ostringstream str; str << value; return str.str(); }
+	template< typename T > string make_str( const T& value ) { std::ostringstream str; str << value; return str.str(); }
 
 	/// convert string to any streamable type
-	template< typename T >
-	T from_string( const std::string& s ) { T value; std::istringstream ostr( s ); str << value; return str.str(); }
-	template<> string from_string< std::string >( const std::string& s ) { return s; }
+	template< typename T > T from_string( const std::string& s ) { T value; std::istringstream ostr( s ); str << value; return str.str(); }
+	template<> inline string from_string< std::string >( const std::string& s ) { return s; }
 
 	/// get a string between quotes
 	inline string quoted( const std::string& s ) { return '\"' + s + '\"'; }
--- flut/system/log.hpp
+++ flut/system/log.hpp
@@ -1,20 +1,27 @@
 #pragma once
 
-#include <iosfwd>
 #include <ostream>
 #include "../string_tools.hpp"
+#include <string>
+#include <iostream>
 
-#define fluc_do_periodic( interval_, statement_ ) { static local_count_ = 0; if ( local_count_++ % interval_ == 0 ) { statement_; } }
+#define flut_do_periodic( interval_, statement_ ) { static local_count_ = 0; if ( local_count_++ % interval_ == 0 ) { statement_; } }
 
-#define FLUC_LOG_TRACE_LEVEL 1
-#define FLUC_LOG_INFO_LEVEL 2
+#define FLUT_LOG_LEVEL_ALL 1
+#define FLUT_LOG_LEVEL_TRACE 1
+#define FLUT_LOG_LEVEL_DEBUG 2
+#define FLUT_LOG_LEVEL_INFO 3
+#define FLUT_LOG_LEVEL_WARNING 4
+#define FLUT_LOG_LEVEL_ERROR 5
+#define FLUT_LOG_LEVEL_CRITICAL 6
+#define FLUT_LOG_LEVEL_NONE 7
 
-#ifndef FLUC_LOG_LEVEL
-#define FLUC_LOG_LEVEL info_level
+#ifndef FLUT_LOG_LEVEL
+#define FLUT_LOG_LEVEL FLUT_LOG_LEVEL_INFO
 #endif
 
-#ifndef FLUC_LOG_OUTPUT_STREAM
-#define FLUC_LOG_OUTPUT_STREAM std::cout
+#ifndef FLUT_LOG_OUTPUT_STREAM
+#define FLUT_LOG_OUTPUT_STREAM std::cout
 #endif
 
 #define flut_logvar( var_ ) std::cout << #var_"=" << var_ << std::endl
@@ -22,10 +29,16 @@
 #define flut_logvar3( var1_, var2_, var3_ ) std::cout << #var1_"=" << var1_ << "\t" << #var2_"=" << var2_ << "\t" << #var3_"=" << var3_ << std::endl
 #define flut_logvar4( var1_, var2_, var3_, var4_ ) std::cout << #var1_"=" << var1_ << "\t" << #var2_"=" << var2_ << "\t" << #var3_"=" << var3_ << "\t" << #var4_"=" << var4_ << std::endl
 
-namespace fluc
+namespace flut
 {
 	namespace log
 	{
+		inline void log_output() { std::cerr << std::endl; }
+
+		template < typename T, typename... Args >
+		void log_output( T var, const Args&... args )
+		{ std::cerr << var; log_output( args... ); }
+
 		struct stream_ignore_wrapper {
 			template< typename T >
 			stream_ignore_wrapper& operator<<( T& value ) const { return *this; }
@@ -41,14 +54,44 @@ namespace fluc
 	}
 }
 
-namespace fluc
+namespace flut
 {
 	namespace log
 	{
-#if ( FLUC_LOG_LEVEL <= FLUC_LOG_TRACE_LEVEL )
-		const auto& trace = output_stream;
-#else
-		const auto& trace = ignore_stream;
+		template< typename... Args > void trace( const Args&... args ) {
+#if ( FLUT_LOG_LEVEL <= FLUT_LOG_LEVEL_TRACE )
+			log_output( args... );
 #endif
+		}
+
+		template< typename... Args > void debug( const Args&... args ) {
+#if ( FLUT_LOG_LEVEL <= FLUT_LOG_LEVEL_DEBUG )
+			log_output( args... );
+#endif
+		}
+
+		template< typename... Args > void info( const Args&... args ) {
+#if ( FLUT_LOG_LEVEL <= FLUT_LOG_LEVEL_INFO )
+			log_output( args... );
+#endif
+		}
+
+		template< typename... Args > void warning( const Args&... args ) {
+#if ( FLUT_LOG_LEVEL <= FLUT_LOG_LEVEL_WARNING )
+			log_output( args... );
+#endif
+		}
+
+		template< typename... Args > void error( const Args&... args ) {
+#if ( FLUT_LOG_LEVEL <= FLUT_LOG_LEVEL_ERROR )
+			log_output( args... );
+#endif
+		}
+
+		template< typename... Args > void critical( const Args&... args ) {
+#if ( FLUT_LOG_LEVEL <= FLUT_LOG_LEVEL_CRITICAL )
+			log_output( args... );
+#endif
+		}
 	}
 }
