f614f7d5356020c0507d1d41a583fc0c26a0c53d
Thomas Geijtenbeek <Thomas.Geijtenbeek@MotekMedical.com>
2015-11-20 17:34:07 +0100

    added flags

---
+++ flut/flags.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace flut
+{
+	template< typename EnumT, typename DataT = unsigned int >
+	struct flags
+	{
+		bool get( EnumT index ) { return ( data & ( 1 << index ) ) != 0; }
+		void set( EnumT index, bool value ) { data = ( data & ~( 1 << index ) ) | ( 1 << index ); }
+		void clear() { data = DataT( 0 ); }
+
+	private:
+		DataT data;
+	};
+}
