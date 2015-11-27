29b0b9c8c9971e045414e4f1087cf53a3d9fec22
Thomas Geijtenbeek <Thomas.Geijtenbeek@MotekMedical.com>
2015-11-27 17:38:56 +0100

    complicated buffer and interpolation stuff

--- flut/storage/interpolation.hpp
+++ flut/storage/interpolation.hpp
@@ -1,20 +1,49 @@
 #pragma once
 
+#include "../math/vec3_type.hpp"
+#include <utility>
+#include <vector>
+#include "buffer_access.hpp"
+#include "../system/assert.hpp"
+#include <xtr1common>
+
 namespace flut
 {
-	template< typename KeyT, typename ValueT >
-	ValueT interpolated_value( const std::vector< std::pair< KeyT, ValueT > >& cont, KeyT key )
+	template< typename T >
+	typename buffer_access<T>::value_t interpolated_value( const T& cont, typename buffer_access<T>::key_t key )
 	{
-		auto ub = std::upper_bound( cont.cbegin(), cont.cend(), key, []( KeyT lhs, const std::pair< KeyT, ValueT >& rhs) { return lhs < rhs.first; } );
+		flut_debug_assert( !cont.empty() );
+
+		auto ub = buffer_access<T>::upper_bound( cont, key );
 		if ( ub == cont.cend() )
-			return cont.back().second;
+			return buffer_access<T>::value_ref( --ub ); // key is beyond data, return last sample
 		else if ( ub == cont.cbegin() )
-			return ub->second;
+			return buffer_access<T>::value_ref( ub ); // key is before data, return first sample
 		else
 		{
-			auto ul = ub - 1;
-			KeyT uw = ( key - ul->first ) / ( ub->first - ul->first );
-			return ( KeyT(1) - uw ) * ul->second + uw * ub->second;
+			// interpolate between two samples (lb and ub)
+			auto lb = ub - 1;
+			buffer_access<T>::key_t uw = ( key - lb->first ) / ( ub->first - lb->first );
+			return ( buffer_access<T>::key_t(1) - uw ) * buffer_access<T>::value_ref( lb ) + uw * buffer_access<T>::value_ref( ub );
+		}
+	}
+
+	template< typename T, typename K >
+	T interpolated_value( const std::vector< T >& cont, K key, typename std::enable_if< !buffer_access< std::vector< T > >::value >::type* = 0 ) 
+	{
+		flut_debug_assert( !cont.empty() );
+
+		if ( key < K(0) )
+			return cont.front();
+		else
+		{
+			auto lb = size_t( key );
+			auto ub = lb + 1;
+			if ( ub >= cont.size() )
+				return cont.back();
+
+			K w = key - lb;
+			return ( K(1) - w ) * cont[ lb ] + w * cont[ ub ];
 		}
 	}
 }
