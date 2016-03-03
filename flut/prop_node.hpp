588e060c54a78c6ac81aa1496e90593a46ec1c51
Thomas Geijtenbeek <Thomas.Geijtenbeek@MotekMedical.com>
2016-03-03 17:43:54 +0100

    initial version of prop_node, added rapidxml as contrib code

---
+++ flut/prop_node.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "types.hpp"
+
+namespace flut
+{
+	template< typename KeyT = string, typename ValueT = string >
+	class prop_node
+	{
+	public:
+		prop_node() {}
+		virtual ~prop_node() {}
+		
+	private:
+		ValueT value;
+		std::vector< std::pair< KeyT, prop_node > > children;
+	};
+}
