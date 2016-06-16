77d4e7452a6883bd8d7409b42e170bc85e5027e6
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2016-06-16 19:13:24 +0200

    math documentation and function name updates

--- flut/math/math.hpp
+++ flut/math/math.hpp
@@ -18,23 +18,33 @@ namespace flut
 			static T one() { return T( 1 ); }
 			static T zero() { return T( 0 ); }
 		};
-
 		const real_t real_pi = constants< real_t >::pi();
 
-		/// handy functions
+		/// convert rad to deg
 		template< typename T > T rad_to_deg( T rad_value ) { return ( T( 180 ) / constants<T>::pi() ) * rad_value; }
+
+		/// convert deg to rad
 		template< typename T > T deg_to_rad( T deg_value ) { return ( constants<T>::pi() / T( 180 ) ) * deg_value; }
+
+		/// compare floating point variables using relaxed_epsilon
 		template< typename T > bool equals( T v1, T v2, T e = constants<T>::relaxed_epsilon() ) { return std::abs( v1 - v2 ) <= std::abs( v1 ) * e; }
-		template< typename T > T inv( T v ) { return T( 1 ) / v; }
-		template< typename T > T invsqrt( T v ) { return T( 1 ) / sqrt( v ); }
+
+		/// inverse of v (1 / v)
+		template< typename T > T inv( T v ) { return T(1) / v; }
+
+		/// inverse of sqrt
+		template< typename T > T invsqrt( T v ) { return T(1) / sqrt( v ); }
+
+		/// sign of v: 1 or -1
 		template< typename T > T sign( T v ) { return v >= T(0) ? T(1) : T(-1); }
-		template< typename T > T between( T v, T min, T max ) { if ( v < min ) return min; else if ( v > max ) return max; else return v; }
-		template< typename T > void limit( T& v, const T& min, const T& max ) {
-			/// TODO: use efficient instructions instead of relying on the compiler for optimization
-			if ( v < min ) v = min;
-			else if ( v > max ) v = max;
-			return v;
-		}
+
+		/// return clamped value that is >= min and <= max
+		template< typename T > T clamped( T v, T min, T max )
+		{ if ( v < min ) return min; else if ( v > max ) return max; else return v; }
+
+		/// clamp a value so that min <= v <= max
+		template< typename T > void clamp( T& v, const T& min, const T& max )
+		{ if ( v < min ) v = min; else if ( v > max ) v = max; return v; } /// TODO: use efficient instructions instead of relying on the compiler for optimization
 
 		/// check if an integer value is a power of two
 		template< typename T > T is_power_of_two( T v ) { return v != 0 && !( v & ( v - 1 ) ); }
