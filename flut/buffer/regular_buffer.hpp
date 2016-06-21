decf505f59415e0a0a24d8a59172d3257f535b07
carmichaelong <carmichael.ong@gmail.com>
2016-06-21 16:24:09 -0700

    buffer fixes to templatization for gcc. now need c++14 (min gcc 4.9)

--- flut/buffer/regular_buffer.hpp
+++ flut/buffer/regular_buffer.hpp
@@ -8,45 +8,45 @@ namespace flut
 	class regular_buffer : public buffer_base< regular_buffer< T, L >, L >
 	{
 	public:
-		regular_buffer( size_t channels ) : buffer_base( channels ), data( channels ) {}
+		regular_buffer( size_t channels ) : buffer_base< regular_buffer< T, L >, L >( channels ), data( channels ) {}
 		~regular_buffer() {}
 
 		/// add empty frame to the buffer
-		void add_frame( T value = T( 0 ) ) { data.insert( data.end(), bufinf.channel_count(), value ); }
+		void add_frame( T value = T( 0 ) ) { data.insert( data.end(), this->channel_count(), value ); }
 
 		/// add a specific frame to the buffer (DO WE NEED THIS?)
 		void add_frame( const std::vector< T >& frame )
-		{ flut_assert( frame.size() == bufinf.channel_count() ); data.insert( data.end(), frame.begin(), frame.end() ); }
+		{ flut_assert( frame.size() == this->channel_count() ); data.insert( data.end(), frame.begin(), frame.end() ); }
 		
 		/// number of frames in the buffer
-		size_t frame_count() { return data.size() / channel_count(); }
+		size_t frame_count() { return data.size() / this->channel_count(); }
 
 		/// set a value for a channel in the most recent frame
-		void set_value( index_t channel, const T& value ) { data[ data.size() - channel_count() + channel ] = value; }
+		void set_value( index_t channel, const T& value ) { data[ data.size() - this->channel_count() + channel ] = value; }
 
 		/// get access to the data in the most recent frame (DO WE NEED THIS?)
 		const T* get_frame( index_t frame )
-		{ flut_assert( frame < frame_count() ); return &data[ frame * channel_count() ]; }
+		{ flut_assert( frame < frame_count() ); return &data[ frame * this->channel_count() ]; }
 
 		/// get the value of a specific frame / channel
 		T get_value( index_t frame, index_t channel )
-		{ flut_assert( frame < frame_count() && channel < channel_count() ); return &data[ frame * channel_count() + channel ]; }
+		{ flut_assert( frame < frame_count() && channel < this->channel_count() ); return &data[ frame * this->channel_count() + channel ]; }
 
 		/// get the interpolated value of a specific frame / channel
 		T get_interpolated_value( index_t frame0, index_t channel, T pos ) {
 			flut_assert( frame0 + 1 < frame_count() );
-			index_t ofs = frame0 * channel_count() + channel;
-			return ( T(1) - pos ) * data[ ofs ] + pos * data[ ofs + channel_count() ];
+			index_t ofs = frame0 * this->channel_count() + channel;
+			return ( T(1) - pos ) * data[ ofs ] + pos * data[ ofs + this->channel_count() ];
 		}
 
 		/// function for internal maintenance, DO NOT CALL this directly
 		/// it is public because of its specific use of CRTP
 		void resize_buffer() {
 			flut_assert_msg( frame_count() <= 1, "Channels can only be added when there's at most one frame in the buffer" );
-			data.resize( channel_count() );
+			data.resize( this->channel_count() );
 		}
 
 	private:
-		std::vector< T > data;
+        std::vector< T > data;
 	};
 }
