066ab82d727ef8ba3785bdf033cd75cfabdde1d5
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2016-08-02 17:14:43 +0200

    documentation updates

--- flut/stopwatch.hpp
+++ flut/stopwatch.hpp
@@ -20,9 +20,13 @@ namespace flut
 		stopwatch() : internal_measure_( 0 ), timer_() { start_first_measure(); }
 		~stopwatch() {}
 
+		/// reset timer for first measurement
 		void start_first_measure() { epoch_ = timer_.ticks(); }
+
+		/// add a measure with a specific tag. identically names measures are summed
 		void add_measure( const string& s );
 
+		/// get report as a prop_node
 		prop_node get_report( int decimals = 6 );
 		
 	private:
