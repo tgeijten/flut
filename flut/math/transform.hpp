03066060aafa1d4ea9451889c8e3725730ac1d67
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2016-08-04 17:51:42 +0200

    transform updates

--- flut/math/transform.hpp
+++ flut/math/transform.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "flut/system/types.hpp"
+#include "flut/math/vec3.hpp"
+#include "flut/math/quat.hpp"
 
 namespace flut
 {
@@ -9,11 +11,13 @@ namespace flut
 		template< typename T >
 		struct transform_
 		{
-			vec3_< T > pos;
-			quat_< T > rot;
+			transform_() {}
+			transform_( const vec3_<T>& p, const quat_<T>& o ) : pos( p ), rot( o ) {}
+			vec3_<T> pos;
+			quat_<T> rot;
 
-			vec3_< T > transformed( const vec3_< T >& v ) { return pos + rot * v; }
-			vec3_< T > inv_transformed( const vec3_< T >& v ) { return -rot * ( v - pos ); }
+			vec3_<T> transformed( const vec3_<T>& v ) { return pos + rot * v; }
+			vec3_<T> inv_transformed( const vec3_<T>& v ) { return -rot * ( v - pos ); }
 		};
 
 		typedef transform_< real_t > transform;
