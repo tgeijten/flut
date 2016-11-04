caa861acba6454784bca414cfe099db5562fa154
carmichaelong <carmichael.ong@gmail.com>
2016-11-03 17:33:19 -0700

    for some random reason major() and minor() are kept as macros on gcc

--- flut/system/version.hpp
+++ flut/system/version.hpp
@@ -9,25 +9,25 @@ namespace flut
 	{
 	public:
 		version( int major, int minor, int maintenance, int build = 0, string postfix = "" ) :
-			major( major ), minor( minor ), maintenance( maintenance ), build( build ), postfix( postfix ) {}
+			majorVer( major ), minorVer( minor ), maintenance( maintenance ), build( build ), postfix( postfix ) {}
 
 		version( const string& version ) {
 			std::stringstream str( version );
 			char dummy;
-			str >> major >> dummy >> minor >> dummy >> maintenance >> dummy;
+			str >> majorVer >> dummy >> minorVer >> dummy >> maintenance >> dummy;
 			if ( str.good() ) str >> build;
 			if ( str.good() ) str >> postfix;
 		}
 
 		string to_str() const {
-			string s = stringf( "%d.%d.%d", major, minor, maintenance );
+			string s = stringf( "%d.%d.%d", majorVer, minorVer, maintenance );
 			if ( build > 0 ) s += stringf( ".%d", build );
 			if ( !postfix.empty() ) s += ' ' + postfix;
 			return s;
 		}
 
-		int major;
-		int minor;
+		int majorVer;
+		int minorVer;
 		int maintenance;
 		int build;
 
