96d0b121479b1a8615e213d366f8856e6d26b49c
TOMMIE-XPS\Thomas <tgeijten@gmail.com>
2017-01-09 10:04:56 +0100

    factory updates (still needs testing)

--- flut/factory.hpp
+++ flut/factory.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <memory>
+#include <functional>
+
 #include "prop_node.hpp"
 
 namespace flut
@@ -9,10 +11,20 @@ namespace flut
 	class factory
 	{
 	public:
-		typedef std::function< controller_info*( const prop_node& ) > create_func_t;
+		typedef std::function< T*( const prop_node& ) > create_func_t;
 
 		// create instance
-		std::unique_ptr< T > operator( const prop_node& pn ) const;
+		std::unique_ptr< T > operator( const prop_node& pn ) const {
+			string type = pn[ "type" ].get< string >();
+			auto it = factory_functions( type );
+			flut_error_if( it == factory_functions.end(), "Unregistered type: " + type );
+			return it->second( pn );
+		}
+
+		// register class subtype
+		void register< T >( const string name ) {
+			factory_functions[ name ] = []( const prop_node& pn ) { return new T( pn ); }
+		}
 
 	private:
 		std::map< string, create_func_t > factory_functions;
