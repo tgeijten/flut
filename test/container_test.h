b52abe7ac565eee51a2e9139c49b34081fbd05ef
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-03-28 12:12:17 +0200

    flut profiler now uses flat storage for better performance and less overhead. updated tests as well.

--- test/container_test.h
+++ test/container_test.h
@@ -2,11 +2,29 @@
 
 #include "flut/vecmap.hpp"
 #include "flut/system/types.hpp"
+#include "flut/sorted_vector.hpp"
 
 namespace flut
 {
 	void container_test()
 	{
+		struct sortable
+		{
+			int e;
+			std::string s;
+			bool operator<( const sortable& other ) const { if ( e == other.e ) return s < other.s; else return e < other.e; }
+		};
+
+		sorted_vector< sortable > sv;
+		sv.insert( sortable{ 3, "peer" } );
+		sv.insert( sortable{ 1, "appel" } );
+		sv.insert( sortable{ 2, "banaan" } );
+		sv.insert( sortable{ 3, "banaan" } );
+		sv.insert( sortable{ 1, "banaan" } );
+
+		for ( auto& e : sv )
+			cout << e.e << e.s << endl;
+
 		vecmap< string, int > vm;
 		vm[ "Appel" ] = 1;
 		vm[ "Peer" ] = 2;
