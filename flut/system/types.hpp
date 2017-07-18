f09ba4e02e1e80de82dd19805c770865ff9e2226
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-07-18 13:12:08 +0200

    added u_ptr and s_ptr

--- flut/system/types.hpp
+++ flut/system/types.hpp
@@ -20,4 +20,6 @@ namespace flut
 	using std::string;
 	using std::pair;
 	using std::vector;
+	template< typename T > using u_ptr = std::unique_ptr< T >;
+	template< typename T > using s_ptr = std::shared_ptr< T >;
 }
