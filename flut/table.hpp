b91daa95c4181081c2575780f4651da9b9b15680
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-11-21 12:37:00 +0100

    removed friends, code cleanup

--- flut/table.hpp
+++ flut/table.hpp
@@ -12,27 +12,27 @@ namespace flut
 	class table
 	{
 	public:
-		table( size_t rows = 0, size_t cols = 0 ) : row_labels( rows ), col_labels( cols ), data( rows * cols ) {}
+		table( size_t rows = 0, size_t cols = 0 ) : row_labels_( rows ), col_labels_( cols ), data_( rows * cols ) {}
 		~table() {}
 
 		index_t add_row( const L& label, const T& default_value = T() ) {
-			row_labels.add( label );
-			data.resize( row_size() * column_size(), default_value );
+			row_labels_.add( label );
+			data_.resize( row_size() * column_size(), default_value );
 			return row_size() - 1;
 		}
 
 		index_t add_column( const L& label, const T& default_value = T() ) {
 			resize( row_size(), column_size() + 1 );
-			return col_labels.set( column_size() - 1, label );
+			return col_labels_.set( column_size() - 1, label );
 		}
 
 		index_t get_or_add_row( const L& label, const T& default_value = T() ) {
-			auto idx = row_labels.find( label );
+			auto idx = row_labels_.find( label );
 			return idx == no_index ? add_row( label, default_value ) : idx;
 		}
 
 		index_t get_or_add_column( const L& label, const T& default_value = T() ) {
-			auto idx = col_labels.find( label );
+			auto idx = col_labels_.find( label );
 			return idx == no_index ? add_column( label, default_value ) : idx;
 		}
 
@@ -40,43 +40,43 @@ namespace flut
 			flut_error_if( rows < row_size() || cols < column_size(), "tables cannot be shrinked" );
 
 			// reorganize existing data
-			data.resize( row_size() * cols );
+			data_.resize( row_size() * cols );
 			for ( index_t ri = rows; ri-- > 0; )
 				for ( index_t ci = cols; ci-- > 0; )
-					data[ cols * ri + ci ] = ( ri < row_size() && ci < column_size() ) ? data[ column_size() * ri + ci ] : T();
-			col_labels.resize( cols );
-			row_labels.resize( rows );
+					data_[ cols * ri + ci ] = ( ri < row_size() && ci < column_size() ) ? data_[ column_size() * ri + ci ] : T();
+			col_labels_.resize( cols );
+			row_labels_.resize( rows );
 		}
 
-		size_t row_size() const { return row_labels.size(); }
-		size_t column_size() const { return col_labels.size(); }
+		size_t row_size() const { return row_labels_.size(); }
+		size_t column_size() const { return col_labels_.size(); }
 
-		const T& operator()( index_t row, index_t col ) const { flut_assert( row < row_size() && col < column_size() ); return data[ row * column_size() + col ]; }
-		T& operator()( index_t row, index_t col ) { flut_assert( row < row_size() && col < column_size() ); return data[ row * column_size() + col ]; }
+		const data_header< L >& row_labels() const { return row_labels_; }
+		const data_header< L >& col_labels() const { return col_labels_; }
+
+		const T& operator()( index_t row, index_t col ) const { flut_assert( row < row_size() && col < column_size() ); return data_[ row * column_size() + col ]; }
+		T& operator()( index_t row, index_t col ) { flut_assert( row < row_size() && col < column_size() ); return data_[ row * column_size() + col ]; }
 
 		const T& operator()( const L& row, const L& col ) const { return (*this)( row_index( row ), col_index( col ) ); }
 		T& operator()( const L& row, const L& col ) { return (*this)( get_or_add_row( row ), get_or_add_column( col ) ); }
 
-		template< typename T1 >
-		friend std::ostream& operator<<( std::ostream& str, const table< T1 >& t );
-
 	private:
 
-		data_header< L > row_labels;
-		data_header< L > col_labels;
-		vector< T > data;
+		data_header< L > row_labels_;
+		data_header< L > col_labels_;
+		vector< T > data_;
 	};
 
 	template< typename T >
 	std::ostream& operator<<( std::ostream& str, const table< T >& t ) {
 		str << "Row";
-		for ( auto& rl : t.col_labels )
+		for ( auto& rl : t.col_labels() )
 			str << "\t" << rl;
 		str << std::endl;
 
 		for ( index_t row = 0; row < t.row_size(); ++row )
 		{
-			str << t.row_labels[ row ];
+			str << t.row_labels()[ row ];
 			for ( index_t col = 0; col < t.column_size(); ++col )
 				str << "\t" << t( row, col );
 			str << std::endl;
--- test/scone/math.h
+++ test/scone/math.h
@@ -43,9 +43,6 @@ namespace scone
 		operator Real&() { return value; }
 		operator const Real&() const { return value; }
 
-		//friend std::ostream& operator<<( std::ostream& str, const Degree& v ) { return str << v.value; }
-		//friend std::istream& operator>>( std::istream& str, Degree& v ) { return str >> v.value; }
-
 		Real value;
 	};
 
